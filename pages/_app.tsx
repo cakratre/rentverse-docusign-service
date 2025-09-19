5ef31793d9094cc4b86134da9d15bb00f63ddf87
Yogaone <yogawanadityapratama@gmail.com>
2025-09-19 20:24:56 +0700

    feat: Update favicon and enhance index page structure with Head component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <title>Rental Agreement PDF Generator</title>
-        <link rel="icon" href="/favicon/icon.svg" type="image/svg+xml" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 // pages/index.tsx
 import DocuSignUploadForm from '@/components/DocuSignUploadForm';
+import Head from 'next/head';
 
 const DocuSignUploadPage = () => {
   return (
-    <div className="min-h-screen bg-[var(--color-background)] py-12">
-      <DocuSignUploadForm />
+    <div className="">
+      <Head>
+        <title>Rental Agreement PDF Generator</title>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </Head>
+      <div className="min-h-screen bg-[var(--color-background)] py-12">
+        <DocuSignUploadForm />
+      </div>
     </div>
+
   );
 }
 
