f7dbb6e74cb20e06ba6fe0c0464d9c08a0de5b39
Yogaone <yogawanadityapratama@gmail.com>
2025-09-20 22:44:20 +0700

    feat: Update CORS configuration to specify allowed origins and improve security

--- utils/cors.ts
+++ utils/cors.ts
@@ -2,16 +2,30 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export function allowCors(handler: (req: NextApiRequest, res: NextApiResponse) => void | Promise<void>) {
   return async (req: NextApiRequest, res: NextApiResponse) => {
-    // Set header CORS
+    // Get the origin from the request
+    const origin = req.headers.origin;
+    
+    // List of allowed origins
+    const allowedOrigins = [
+      'https://rentverse-by-cakratre.vercel.app',
+      'https://rentverse-ra.yogaone.me'
+    ];
+
+    // Set CORS headers
+    if (origin && allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    } else {
+      res.setHeader("Access-Control-Allow-Origin", "https://rentverse-by-cakratre.vercel.app");
+    }
+    
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Origin", "*"); // allow all
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET,OPTIONS,PATCH,DELETE,POST,PUT"
     );
     res.setHeader(
       "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
     );
 
     // Handle preflight request
